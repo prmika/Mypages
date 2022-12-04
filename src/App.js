1d2e5ddb7dbfcb9371eca91d04a6c6e968bca1e1
prmika <miika.kauppinen@tuni.fi>
2022-12-04 18:09:53 +0000

    added_navbar

--- src/App.js
+++ src/App.js
@@ -1,34 +1,20 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import { Router, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1>
-          Miika Kauppinen
-        </h1>
-      </header>
-      {/*todo: make boxes for different pages*/}
-      <div
-      className='App-sidebar'
-      >
-      <a>About me</a>
-      <a>Games?</a>
-      <a>Something</a>
-      </div>
- 
- 
- 
- 
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </div>
+    <>
+        <Navbar />
+      {/*<Router>
+        <Routes>
+          <Route path='/' exact />
+
+        </Routes>
+      </Router>*/}
+
+    </>
   );
 }
 
