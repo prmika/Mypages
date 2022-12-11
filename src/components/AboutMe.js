e567f0895962c8a23dfb9377dbef637f584d2d6d
prmika <miika.kauppinen@tuni.fi>
2022-12-11 15:45:34 +0000

    Added_AboutMe

---
+++ src/components/AboutMe.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+function AboutMe() {
+    const [click, setClick] = useState(false);
+
+    
+    return (
+        <>
+            <div className='AboutMe'>
+                perkele
+            </div>
+        </>
+    )
+}
+
+export default AboutMe
